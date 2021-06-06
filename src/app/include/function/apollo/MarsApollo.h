0829ae41d954014aa82dc1674fda4a8c0d8c06c2
LeiZhang-Hunter <357733652@qq.com>
2021-06-06 17:30:11 +0800

    apollo配置文件加载

--- src/app/include/function/apollo/MarsApollo.h
+++ src/app/include/function/apollo/MarsApollo.h
@@ -4,18 +4,32 @@
 
 #ifndef MARS_AGENT_MARSAPOLLO_H
 #define MARS_AGENT_MARSAPOLLO_H
+
 #include <memory>
 
 #include "MarsFunctionObject.h"
 #include "MarsApolloConfig.h"
+
 namespace app {
     class NodeAgent;
 }
 namespace function {
     namespace apollo {
-    class MarsApollo : public MarsFunctionObject, public std::enable_shared_from_this<MarsApollo>{
+        class MarsApollo : public MarsFunctionObject, public std::enable_shared_from_this<MarsApollo> {
         public:
             MarsApollo(const std::shared_ptr<app::NodeAgent> &agent);
+
+            void initFunction() {
+
+            }
+
+            void finishFunction() {
+
+            }
+
+            void shutdownFunction() {
+
+            }
 
         private:
             std::shared_ptr<MarsApolloConfig> apolloConfig;
